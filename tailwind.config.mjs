574b29eb6a88d2a3e38dbe491045ec072259959d
yutong-dai <yutong.dai@salesforce.com>
2025-09-30 10:22:48 -0700

    add theme toggle

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
 		extend: {},
-	},
-	plugins: [require('@tailwindcss/typography'),],
-	theme: {
 		fontFamily: {
 			'sans': ['Noto Sans']
 		}
-	}
+	},
+	plugins: [require('@tailwindcss/typography')],
 }
